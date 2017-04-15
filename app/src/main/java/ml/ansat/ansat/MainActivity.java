ff4c6f8a05e9c0d1921c84030bdef7f321871143
Chirag Arora <chirag.arora2015@vit.ac.in>
2017-04-16 00:56:23 +0530

    Home and Dashboard XML

--- app/src/main/java/ml/ansat/ansat/MainActivity.java
+++ app/src/main/java/ml/ansat/ansat/MainActivity.java
@@ -3,9 +3,12 @@ package ml.ansat.ansat;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import ml.ansat.ansat.Home.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,13 +21,22 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    HomeFragment homeFragment = new HomeFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.content, homeFragment,"HOME_FRAGMENT");
+                    fragmentTransaction.commit();
                     return true;
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_profile:
+                    mTextMessage.setText(R.string.title_profile);
+                    return true;
+                case R.id.navigation_blog:
+                    mTextMessage.setText(R.string.title_blog);
+                    return true;
+                case R.id.navigation_logout:
+                    mTextMessage.setText(R.string.title_logout);
                     return true;
             }
             return false;
@@ -36,6 +48,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HomeFragment homeFragment = new HomeFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content, homeFragment,"HOME_FRAGMENT");
+        fragmentTransaction.commit();
+
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
---
+++ app/src/main/java/ml/ansat/ansat/SplashScreen/Splash.java
@@ -0,0 +1,55 @@
+package ml.ansat.ansat.SplashScreen;
+
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import ml.ansat.ansat.MainActivity;
+import ml.ansat.ansat.R;
+
+public class Splash extends AppCompatActivity {
+
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 5000) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(Splash.this,
+                            MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    Splash.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    Splash.this.finish();
+                }
+
+            }
+        };
+        thread.start();
+    }
+}
+
